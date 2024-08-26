c774ba6e5ac12e0b193464fd56c1611254d6ac66
Lucas Almeida <lucasmirandaalmeida2@gmail.com>
2024-08-26 12:59:12 -0400

    Desafio Concluido!

--- DesafioControleFluxo/src/Contador.java
+++ DesafioControleFluxo/src/Contador.java
@@ -1,5 +1,39 @@
-public class Contador {
+
+import java.util.Scanner;
+
+public class Contador extends ParametrosInvalidosException {
+
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Bem vindo a aplicação de contagem numerica! \nPara começarmos você deve passar dois parametros inteiros sendo o primeiro menor que o segundo ");
+        System.out.println("Primeiro parametro: ");
+        int parametroUm = scanner.nextInt();
+        System.out.println("Segundo parametro: ");
+        int parametroDois = scanner.nextInt();
+
+        try {
+            contar(parametroUm, parametroDois);
+        } catch (ParametrosInvalidosException e) {
+            System.out.println(e + "\nO segundo parâmetro deve ser maior que o primeiro ");
+        }
+
+
+       
+
     }
+
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+        if (parametroUm > parametroDois) {
+            throw new ParametrosInvalidosException();
+
+        } else {
+
+            for (int i = parametroUm; i <= parametroDois; i++) {
+                System.out.println("Imprimindo o número: " + i);
+            }
+
+        }
+    }
+
 }
